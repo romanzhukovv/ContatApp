ce61e284eb9af9535b7983f2ab5ea6d032f1eb9e
romanzhukovv <zhukov.roman89@gmail.com>
2021-12-28 23:06:04 +0300

    created storyboard

--- ContatApp/ContactsListViewController.swift
+++ ContatApp/ContactsListViewController.swift
@@ -23,23 +23,21 @@ class ContactsListViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 7
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        cell.textLabel?.text = "fsdfsdf"
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
--- ContatApp/PersonsListViewController.swift
+++ ContatApp/PersonsListViewController.swift
@@ -21,25 +21,20 @@ class PersonsListViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
+        cell.textLabel?.text = "fsdfsdf"
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
